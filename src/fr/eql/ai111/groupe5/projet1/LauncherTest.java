0ef5515d6ca107e8e329e0c4636c1e9dca45e598
Tim <timothee.loisel@gmail.com>
2022-06-02 15:54:42 +0200

    Creation method returning list of strings from .txt + gitIgnore added

--- src/fr/eql/ai111/groupe5/projet1/LauncherTest.java
+++ src/fr/eql/ai111/groupe5/projet1/LauncherTest.java
@@ -2,8 +2,6 @@ package fr.eql.ai111.groupe5.projet1;
 
 public class LauncherTest {
 
-    RafExecute rafExecute = new RafExecute();
-
     public static void main(String[] args) {
 
 
--- src/fr/eql/ai111/groupe5/projet1/RafExecute.java
+++ src/fr/eql/ai111/groupe5/projet1/RafExecute.java
@@ -1,10 +1,10 @@
 package fr.eql.ai111.groupe5.projet1;
 
-import fr.eql.ai111.groupe5.projet1.Stagiaire;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -17,33 +17,95 @@ public class RafExecute {
     LogManager logManager;
     Logger logger;
 
-    public void createStagiairesObjects() throws IOException {
+    public List<String> createListStagiaires() throws IOException {
 
-        FileReader reader = new FileReader("C:/Users/Formation/Desktop/PROJET1/Stagiaires.txt");
+        FileReader reader = null;
+        try {
+            reader = new FileReader("C:/Users/Formation/Desktop/PROJET1/Stagiaires.txt");
+        } catch (FileNotFoundException e) {
+            logger.warn("Le fichier à lire n'existe pas.");
+        }
         BufferedReader bfReader = new BufferedReader(reader);
-        RandomAccessFile raf = new RandomAccessFile("C:/Users/Formation/Desktop/PROJET1/Raf.bin", "rw");
-        List<Stagiaire> listStagiaires = new ArrayList<>();
+        List<String> listStringStagiaires = new ArrayList<>();
+        String stagiaireX = "";
+        String espace = " ";
+        int index = 0;
+
+        String surname = "";
+        String name = "";
+        String dept = "";
+        String promo = "";
+        String year = "";
+        String temp = "";
 
         while (bfReader.ready()){
-            bfReader.readLine();
-            String chain = bfReader.readLine();
-            Stagiaire stagiaire = null;
-            StringTokenizer st = new StringTokenizer(chain, "\r\n");
-            if(st.countTokens()==5){
-                String surname = st.nextToken();
-                String name = st.nextToken();
-                String promo = st.nextToken();
-                String dept = st.nextToken();
-                String year = st.nextToken();
-                stagiaire = new Stagiaire(surname, name, dept, promo, year);
-                listStagiaires.add(stagiaire);
-                System.out.println(stagiaire);
-
+            index++;
+            temp = "";
+            temp = bfReader.readLine();
+            switch(index){
+                case 1 :
+                    surname = temp;
+                    int nbrEspace = 50 - surname.length();
+                    for (int i = 0; i < nbrEspace; i++){
+                        surname+= espace;
+                    }
+                    break;
+                case 2 :
+                    name = temp;
+                    nbrEspace = 50 - name.length();
+                    for (int i = 0; i < nbrEspace; i++){
+                        name+= espace;
+                    }
+                    break;
+                case 3 :
+                    dept = temp;
+                    nbrEspace = 3 - dept.length();
+                    for (int i = 0; i < nbrEspace; i++){
+                        dept+= espace;
+                    }
+                    break;
+                case 4 :
+                    promo = temp;
+                    nbrEspace = 20 - promo.length();
+                    for (int i = 0; i < nbrEspace; i++){
+                        promo+= espace;
+                    }
+                    break;
+                case 5:
+                    year = temp;
+                    break;
+                case 6:
+                    stagiaireX = surname + " " + name + " " + dept + " " + promo + " " + year ;
+                    listStringStagiaires.add(stagiaireX);
+                    index = 0;
+                    System.out.println(stagiaireX);
+                    surname = "";
+                    name = "";
+                    dept = "";
+                    promo = "";
+                    year = "";
+                    break;
+            }
         }
+        return listStringStagiaires;
+    }
 
+    public void writeIntoRaf (List<String> listStringStagiaires){
+        try {
+            RandomAccessFile rafDataBase =
+                    new RandomAccessFile("C:/Users/Formation/Desktop/PROJET1/Raf.bin", "rw");
+            for (String lss : listStringStagiaires) {
+                String tempLss = lss;
+                rafDataBase.writeChars(tempLss);
+            }
+        } catch (IOException e) {
+            logger.warn("Le chemin de création du fichier n'existe pas ou le fichier ne contient pas de données.");
+        }
 
     }
 
-
-}
+    public static void main(String[] args) throws IOException {
+        RafExecute rafExecute = new RafExecute();
+        rafExecute.createListStagiaires();
+    }
 }
