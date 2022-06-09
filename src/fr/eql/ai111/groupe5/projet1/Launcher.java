6224401687db0f92220b3d392a696c0015183647
Sab-Ben <sbenseghir.sb@gmail.com>
2022-06-09 09:59:39 +0200

    1st commit - interface, MAJ

--- src/fr/eql/ai111/groupe5/projet1/Launcher.java
+++
@@ -1,9 +0,0 @@
-package fr.eql.ai111.groupe5.projet1;
-
-public class Launcher {
-
-    public static void main(String[] args) {
-        Connexion connect = new Connexion();
-        connect.execute();
-    }
-}
