75fb0daf24e0dcb6fe1f3659930017949f25e617
Antoine Maffay <amaffray1@gmail.com>
2022-06-09 09:29:07 +0200

    Parcours de l'arbre quasi fini (gérer l'exception qui arrête la méthode) et début de l'export en pdf + fonction de hashage dans methods + début export

--- src/fr/eql/ai111/groupe5/projet1/TextExport.java
+++ src/fr/eql/ai111/groupe5/projet1/TextExport.java
@@ -1,38 +1,52 @@
-package fr.eql.ai111.groupe5.projet1;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import java.io.IOException;
-import java.util.ArrayList;
-
-public class TextExport {
-    public static void main(String[] args) throws IOException {
-
-        ArrayList<String> testeuse = new ArrayList<>();
-
-        testeuse.add("Bonjour" + "\r\n");
-        testeuse.add("Aurevoir");
-        System.out.println(testeuse);
-
-        try{
-        PDDocument document = new PDDocument();
-        PDPage page = new PDPage();
-        document.addPage(page);
-
-        PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
 
-        contentStream.beginText();
-        contentStream.showText("hello");
-        contentStream.endText();
-        contentStream.close();
 
-        document.save("C:/FolderProjet/pdftest.pdf");
-        document.close();
-        }catch(Exception e){
-            System.out.println(e);
-        }
 
-    }
-}
+
+
+
+
+
+//package fr.eql.ai111.groupe5.projet1;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import org.apache.pdfbox.pdmodel.PDDocument;
+//import org.apache.pdfbox.pdmodel.PDPage;
+//import org.apache.pdfbox.pdmodel.PDPageContentStream;
+//import org.apache.pdfbox.pdmodel.font.PDType1Font;
+//import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
+//
+//public class TextExport {
+//    public static void main(String[] args) throws IOException{
+//        File file = new File("C:/FolderProjet/test.pdf");
+//        PDDocument document = new PDDocument();
+//        document.save(file);
+//        PDPage page = document.getPage(0);
+//        PDType1Font pdType1Font = new PDType1Font(Standard14Fonts.FontName.TIMES_BOLD);
+//        PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND,true,true);
+//        contentStream.beginText();
+//
+//        //Setting the font
+//        contentStream.setFont(pdType1Font, 12);
+//
+//        //Setting the text position
+//        contentStream.newLineAtOffset(25, 500);
+//
+//        String text = "This message is writtern to the pdf file.";
+//        contentStream.showText(text);
+//        contentStream.endText();
+//        contentStream.close();
+//        // Saving file after writing
+//        document.save(new File("test.pdf"));
+//        document.close();
+//    }
+//}
+//
+
+//    ArrayList<String> testeuse = new ArrayList<>();
+//
+//        testeuse.add("Bonjour" + "\r\n");
+//                testeuse.add("Aurevoir");
+//                System.out.println(testeuse);
+//
