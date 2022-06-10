04d636a457f829b022038533c42b9d23a846c97b
Antoine Maffay <amaffray1@gmail.com>
2022-06-10 09:49:56 +0200

    ajout export pdf dans methods + modif hasahage

--- src/fr/eql/ai111/groupe5/projet1/TextExportPdfbox.java
+++ src/fr/eql/ai111/groupe5/projet1/TextExportPdfbox.java
@@ -1,13 +1,11 @@
 package fr.eql.ai111.groupe5.projet1;
 
-import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import fr.eql.ai111.groupe5.projet1.methodsback.Arbre;
-import fr.eql.ai111.groupe5.projet1.methodsback.Methods;
 import fr.eql.ai111.groupe5.projet1.methodsback.Stagiaire;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -16,15 +14,14 @@ import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
-import org.apache.pdfbox.pdmodel.graphics.image.PDImage;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 public class TextExportPdfbox {
     public static void main(String[] args) throws IOException{
 
-        List<Stagiaire> testeuse = new ArrayList<>();
+        List<Stagiaire> stagiaires = new ArrayList<>();
         Arbre arbre = new Arbre();
-        testeuse = arbre.arbreParcours();
+        stagiaires = arbre.arbreParcours();
         int compteur = 10;
         PDDocument doc = null;
         try
@@ -49,7 +46,7 @@ public class TextExportPdfbox {
             contentStream.beginText();
             contentStream.setFont(pdfFont, fontSize);
             contentStream.newLineAtOffset(30, 550);
-            for (Stagiaire s : testeuse) {
+            for (Stagiaire s : stagiaires) {
                 int nspace1 = 20 - (s.getSurname().trim().length());
                 String space1 = "";
                 text = "";
@@ -77,7 +74,9 @@ public class TextExportPdfbox {
             }
             contentStream.endText();
             contentStream.close();
-            doc.save(new File("C:/FolderProjet/test.pdf"));
+            File file = new File("C:/FolderProjet/test.pdf");
+            if (!file.exists());
+            doc.save(file);
         }
         finally
         {
@@ -89,82 +88,3 @@ public class TextExportPdfbox {
 
     }
 }
-
-//                for (int i = 0; i < nspace1; i++) {
-//                    space1 += " ";
-//                }
-//                int nspace2 = 15 - (s.getName().trim().length());
-//                String space2 = "";
-//                for (int i = 0; i < nspace2; i++) {
-//                    space2 += " ";
-//                }
-//                int nspace3 = 5 - (s.getDept().trim().length());
-//                String space3 = "";
-//                for (int i = 0; i < nspace1; i++) {
-//                    space3 += " ";
-//                }
-//                int nspace4 = 10 - (s.getPromo().trim().length());
-//                String space4 = "";
-//                for (int i = 0; i < nspace1; i++) {
-//                    space4 += " ";
-//                }
-//                int spaceIndex = text.indexOf(' ', lastSpace + 1);
-//                if (spaceIndex < 0)
-//                    spaceIndex = text.length();
-//                String subString = text.substring(0, spaceIndex);
-//                float size = fontSize * pdfFont.getStringWidth(subString) / 1000;
-//                System.out.printf("'%s' - %f of %f\n", subString, size, width);
-//                if (size > width)
-//                {
-//                    if (lastSpace < 0)
-//                        lastSpace = spaceIndex;
-//                    subString = text.substring(0, lastSpace);
-//                    lines.add(subString);
-//                    text = text.substring(lastSpace).trim();
-//                    System.out.printf("'%s' is line\n", subString);
-//                    lastSpace = -1;
-//                }
-//                else if (spaceIndex == text.length())
-//                {
-//                    lines.add(text);
-//                    System.out.printf("'%s' is line\n", text);
-//                    text = "";
-//                }
-//                else
-//                {
-//                    lastSpace = spaceIndex;
-//                }
-
-//            contentStream.beginText();
-//            contentStream.setFont(pdfFont, fontSize);
-//            contentStream.newLineAtOffset(startX, startY);
-//            for (String line: lines)
-//            {
-//                contentStream.showText(line);
-//                contentStream.newLineAtOffset(0, -leading);
-//            }
-
-
-
-//    File file = new File("C:/FolderProjet/test.pdf");
-//    PDDocument document = new PDDocument();
-//    PDPage page = new PDPage();
-//    PDType1Font pdType1Font = new PDType1Font(Standard14Fonts.FontName.TIMES_BOLD);
-//    PDPageContentStream contentStream = new PDPageContentStream(document, page, PDPageContentStream.AppendMode.APPEND,true,true);
-//        contentStream.beginText();
-//
-//                page.getActions();
-//
-//                //Setting the font
-//                contentStream.setFont(pdType1Font, 12);
-//
-//                //Setting the text position
-//                contentStream.newLineAtOffset(25, 500);
-//
-//                String text = "This message is writtern to the pdf file.";
-//                contentStream.showText(text);
-//                contentStream.endText();
-//                contentStream.close();
-//                // Saving file after writing
-//                document.save("C:/FolderProjet/test.pdf");
-//                document.close();
--- src/fr/eql/ai111/groupe5/projet1/methodsback/Methods.java
+++ src/fr/eql/ai111/groupe5/projet1/methodsback/Methods.java
@@ -10,6 +10,15 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.File;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 public class Methods {
 
@@ -322,8 +331,8 @@ public class Methods {
 
     public String hashage (String mdp) {
         String hashed = "";
-
         long hasher = 99991;
+        mdp += mdp + "KeY";
         for (int i = 0; i < mdp.length(); i++) {
             switch (mdp.charAt(i)) {
                 case 'a' : hasher += hasher *307;break;
@@ -444,4 +453,73 @@ public class Methods {
         return hashed;
     }
 
+    public void TextExportPdfbox (List<Stagiaire> stagiaires, String fileName) throws IOException {
+
+        Arbre arbre = new Arbre();
+        stagiaires = arbre.arbreParcours();
+        int compteur = 10;
+        PDDocument doc = null;
+        try
+        {
+            doc = new PDDocument();
+            PDPage page = new PDPage();
+            doc.addPage(page);
+            PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+            PDImageXObject image = PDImageXObject.createFromFile("C:/FolderProjet/eql.png", new PDDocument());
+            PDImageXObject image2 = PDImageXObject.createFromFile("C:/FolderProjet/eqlfil.png", new PDDocument());
+            contentStream.drawImage(image, 30, 580);
+            PDFont pdfFont = new PDType1Font(Standard14Fonts.FontName.HELVETICA);
+            // taille des caractères
+            float fontSize = 12;
+            // interligne
+            float leading = 1.5f * fontSize;
+            PDRectangle mediabox = page.getMediaBox();
+            // marge gauche
+            float margin = 20;
+            String text = "";
+            StringBuffer stringBuffer = new StringBuffer();
+            contentStream.beginText();
+            contentStream.setFont(pdfFont, fontSize);
+            contentStream.newLineAtOffset(30, 550);
+            for (Stagiaire s : stagiaires) {
+                int nspace1 = 20 - (s.getSurname().trim().length());
+                String space1 = "";
+                text = "";
+                stringBuffer.append(s.getSurname().trim()+s.getName().trim()+ s.getDept().trim()+ s.getPromo().trim()+ s.getYear().trim());
+                text = "Nom: " + s.getSurname().trim()+ "  " +
+                        "Prénom: " + s.getName().trim()+ "  " +
+                        "Département: " + s.getDept().trim()+ "  " +
+                        "Promotion: " + s.getPromo().trim()+ "  " +
+                        "Année: " + s.getYear().trim();
+                compteur = compteur+1;
+                if (compteur > 40) {
+                    page = new PDPage();
+                    doc.addPage(page);
+                    contentStream.endText();
+                    contentStream.close();
+                    contentStream = new PDPageContentStream(doc, page);
+                    contentStream.drawImage(image2, 150, 150);
+                    contentStream.beginText();
+                    contentStream.setFont(pdfFont, fontSize);
+                    contentStream.newLineAtOffset(20, 750);
+                    compteur = 0;
+                }
+                contentStream.showText(text);
+                contentStream.newLineAtOffset(0, -leading);
+            }
+            contentStream.endText();
+            contentStream.close();
+            File file = new File("C:/theEQLBook/"+fileName+".pdf");
+            if (!file.exists()) {
+                doc.save(file);
+            }
+        }
+        finally
+        {
+            if (doc != null)
+            {
+                doc.close();
+            }
+        }
+    }
 }
